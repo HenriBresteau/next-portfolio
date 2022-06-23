68e1339cb3d6cb48aaa8e8ab07f8d9d33ad17f7e
Henri Bresteau <henri.bresteau@gamil.com>
2022-06-23 15:35:35 +0200

    test page missive

---
+++ pages/missive.js
@@ -0,0 +1,12 @@
+import { Container, Heading } from '@chakra-ui/react'
+
+const Missive = () => {
+  return (
+    <Container>
+      <Heading as="h2" fontSize={24} mb={4}>
+        Missive
+      </Heading>
+    </Container>
+  )
+}
+export default Missive
